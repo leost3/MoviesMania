5251b2a7200bf5f9126ffe4805fe8d7467f595b1
leost3 <leost3@gmail.com>
2019-07-09 08:49:42 -0400

    fetching movies detail by axios

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 // import { Link } from 'react-router-dom';
 
 class Movie extends React.Component {
+    state = {movieDetails: []}
+    async componentDidMount() {
+        const KEY = 'f94e9a18c1c262bae36e6cdc7be57a1d';
+        // const getMovieById = `https://api.themoviedb.org/3/movie/550?api_key=${KEY}`;
+        const getMovieByTrend = `https://api.themoviedb.org/3/movie/${this.props.match.params.movieId}?api_key=${KEY}`;
+
+        const response = await axios(getMovieByTrend);
+        // const configResp = await axios(getConfig);
+        const data = response.data;
+        console.log(data);
+    }
 
     render() {
 
